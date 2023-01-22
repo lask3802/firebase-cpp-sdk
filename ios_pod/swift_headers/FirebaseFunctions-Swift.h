522b61e1ed8e5d63e13ebbc459e14f1746b41679
Jon Simantov <jsimantov@google.com>
2023-01-22 00:04:31 -0800

    Format code.

--- ios_pod/swift_headers/FirebaseFunctions-Swift.h
+++ ios_pod/swift_headers/FirebaseFunctions-Swift.h
@@ -222,9 +222,9 @@ typedef unsigned int swift_uint4 __attribute__((__ext_vector_type__(4)));
 #pragma push_macro("any")
 #undef any
 #pragma clang attribute push(                                                                   \
-    __attribute__((external_source_symbol(language = "Swift", defined_in = "FirebaseFunctions", \
-                                          generated_declaration))),                             \
-    apply_to = any(function, enum, objc_interface, objc_category, objc_protocol))
+        __attribute__((external_source_symbol(                                                  \
+                language = "Swift", defined_in = "FirebaseFunctions", generated_declaration))), \
+            apply_to = any(function, enum, objc_interface, objc_category, objc_protocol))
 #pragma pop_macro("any")
 #endif
 
